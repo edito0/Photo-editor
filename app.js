8de5412d70c8b1e6c12c3e6a08c0a3fb191d3376
Mohit Bagdi <92544103+edito0@users.noreply.github.com>
2022-04-29 12:45:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,76 @@
+var foto;
+window.onload = function() {
+    foto = new Foto();
+}
+
+function selectImage() {
+    document.getElementById('foto-file').click();
+}
+
+function makeBright(){
+    foto.makeBright();
+}
+
+function makeGrayScale(){
+    foto.grayscale();
+}
+
+function makeDark(){
+    foto.makeDark();
+}
+
+function makeBlur(){
+    foto.applyBlurFilter();
+}
+
+function makeEmboos(){
+    foto.applyEmbossFilter();
+}
+
+function makeSharp(){
+    foto.applySharpFilter();
+}
+
+function download()  
+{
+    foto.export(); 
+}
+
+
+function openColorpicker(){
+    document.getElementById("color-picker").click() 
+    ;
+}
+
+function makeColorize(elem){
+    var color = elem.value;
+
+    foto.colorize(color);
+}
+
+function openColorizeColorPicker(){
+    document.getElementById("colorize-color-picker").click() 
+    ;
+}
+
+function makeTransparent(){
+    foto.makeTransparent();
+}
+
+function applyColorFilter(elem){
+    var color = elem.value;
+    foto.applyColorFilter(color);
+}
+
+function crop(){
+    foto.cropSelected();
+}
+
+function flipvertically(){
+    foto.flipVertically();
+}
+
+
+function rotate(elem){
+    foto.rotate(elem.value);
+}
